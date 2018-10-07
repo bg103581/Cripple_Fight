288d8b2c4aa9733dbe94d5cdcd9793f2879907f3
Byzness <guillaume.briant@free.fr>
2018-10-08 01:24:22 +0200

    Petit  / grand jump (tap/reste appuyer)

--- CrippleFight/Assets/Scripts/BasicMoveRyu.cs
+++ CrippleFight/Assets/Scripts/BasicMoveRyu.cs
@@ -4,35 +4,52 @@ using UnityEngine;
 
 public class BasicMoveRyu : MonoBehaviour {
 
-    public int speedX; // speed de déplacement de Ryu
-    public int speedY; // speed de jump
+    public int speed; // speed de déplacement de Ryu
+    public int jumpForce; // speed de jump
+    public float jumpTime, jumpTimeCounter;
     public Rigidbody2D rbRyu; // pour sauter
     private bool isJumping = false;
+    private bool stoppedJumping = true;
 
 	// Use this for initialization
 	void Start () {
-		
+        jumpTimeCounter = jumpTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey("right") && transform.position.x <= 10.45f) {
-            transform.Translate(Vector2.right * speedX * Time.deltaTime); // faire avancer Ryu à droite
+            transform.Translate(Vector2.right * speed * Time.deltaTime); // faire avancer Ryu à droite
         }
 
         if (Input.GetKey("left") && transform.position.x >= -10.40f) {
-            transform.Translate(Vector2.left * speedX * Time.deltaTime); // faire avancer Ryu à gauche
+            transform.Translate(Vector2.left * speed * Time.deltaTime); // faire avancer Ryu à gauche
         }
 
         if (Input.GetKeyDown("space") && !isJumping) { // faire sauter Ryu
-            rbRyu.velocity = Vector2.up * speedY;
+            rbRyu.velocity = new Vector2(rbRyu.velocity.x, jumpForce);
             isJumping = true;
+            stoppedJumping = false;
+        }
+
+        if (Input.GetKey("space") && !stoppedJumping) { // augmente le jump quand rester appuyer
+            if (jumpTimeCounter > 0) {
+                rbRyu.velocity = new Vector2(rbRyu.velocity.x, jumpForce);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+        }
+
+        if (Input.GetKeyUp("space")) { // empeche de reaugmenter le jump après stop jump
+            jumpTimeCounter = 0;
+            stoppedJumping = true;
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "ground") {
             isJumping = false;
+            jumpTimeCounter = jumpTime;
+            stoppedJumping = true;
         }
     }
 }
