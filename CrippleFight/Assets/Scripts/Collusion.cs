59733301f0046095a56f24a28e2d77f74d80566a
hakimloukil <loukilhakim@outlook.com>
2018-10-09 15:34:28 +0200

    Collider

---
+++ CrippleFight/Assets/Scripts/Collusion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collusion : MonoBehaviour {
+    public GameObject Col;
+    
+    Animator AnimatorPlayer;
+    // Use this for initialization
+    void Start () {
+        AnimatorPlayer = GetComponent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (AnimatorPlayer.GetBool("isKicking") == true)
+        {
+            Col.SetActive(true);
+        }
+        else {
+            Col.SetActive( false);
+        }
+	}
+}
