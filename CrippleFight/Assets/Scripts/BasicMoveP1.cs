e91633e2b9fb6e90c1e7952b48b5965a22371f82
Byzness <guillaume.briant@free.fr>
2018-10-11 00:33:54 +0200

    Jump control + dash

--- CrippleFight/Assets/Scripts/BasicMoveP1.cs
+++ CrippleFight/Assets/Scripts/BasicMoveP1.cs
@@ -6,48 +6,82 @@ public class BasicMoveP1 : MonoBehaviour {
 
     public int speed; // speed de déplacement de Ryu
     public int jumpForce; // speed de jump
+    private int rightPress, leftPress; // nb de fois qu'on a appué sur left ou right
     public float jumpTime; //nb de sec max du jump
     public float jumpTimeCounter; // compteur du temps de jump
+    private float delay, delayPress, timePassed, timePassedPress, dashTime, dashTimeCounter;
     public Rigidbody2D rbRyu; // pour sauter
-    private bool isJumping = false;
+    private bool isJumping = false, startTimer, startDelay, grounded, isRight, isLeft, isDashingRight, isDashingLeft;
     public Animator ryuAnimator;
-    Vector2 Pos,Max;
+    Vector2 Pos, Max;
 
 
     // Use this for initialization
     void Start() {
         jumpTimeCounter = jumpTime;
         ryuAnimator = GetComponent<Animator>();
+        rightPress = leftPress = 0;
+        timePassed = timePassedPress = dashTimeCounter = 0;
+        delayPress = 0.25f;
+        delay = 0.25f;
+        dashTime = 0.1f;
+        isDashingLeft = isDashingRight = isRight = isLeft = false;
         Vector3 Cam = Camera.main.transform.position;
-         Max = Camera.main.ScreenToWorldPoint(Cam);
-        
+        Max = Camera.main.ScreenToWorldPoint(Cam);
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKey("d") && ryuAnimator.GetBool("isCrouching") == false) {
+        if (Input.GetKeyDown("d")) {
+            rightPress++;
+            startTimer = true;
+        } else if (Input.GetKeyDown("q")) {
+            leftPress++;
+            startTimer = true;
+        }
+
+        if (startTimer) {
+            timePassedPress += Time.deltaTime;
+            if (timePassedPress >= delayPress) {
+                startTimer = false;
+                rightPress = 0;
+                leftPress = 0;
+                timePassedPress = 0;
+            }
+        }
+
+        if (leftPress >= 2 || rightPress >= 2) {
+            startDelay = true;
+        }
+
+        if (Input.GetKey("d") && ryuAnimator.GetBool("isCrouching") == false && grounded) {
             rbRyu.transform.rotation = new Quaternion(0, 0f, 0f, 0f);
             transform.Translate(Vector2.right * speed * Time.deltaTime); // faire avancer Ryu à droite
             Pos = transform.position;
             Pos.x = Mathf.Clamp(Pos.x, Max.x - Max.x / 10, -Max.x + Max.x / 10);
             transform.position = Pos;
             ryuAnimator.SetBool("isWalking", true);
+            isRight = true;
+            isLeft = false;
 
-        } else if (Input.GetKey("q") && ryuAnimator.GetBool("isCrouching") == false) {
+        } else if (Input.GetKey("q") && ryuAnimator.GetBool("isCrouching") == false && grounded) {
             rbRyu.transform.rotation = new Quaternion(0, 180f, 0f, 0f);
             transform.Translate(-Vector2.left * speed * Time.deltaTime); // faire avancer Ryu à gauche
             Pos = transform.position;
-            Pos.x = Mathf.Clamp(Pos.x, Max.x-Max.x/10, -Max.x+Max.x/10);
+            Pos.x = Mathf.Clamp(Pos.x, Max.x - Max.x / 10, -Max.x + Max.x / 10);
             transform.position = Pos;
             ryuAnimator.SetBool("isWalking", true);
+            isLeft = true;
+            isRight = false;
         } else {
             ryuAnimator.SetBool("isWalking", false);
         }
 
-        if (Input.GetKeyDown("z") && (jumpTimeCounter == jumpTime)) { // faire sauter Ryu
+        if (Input.GetKeyDown("z") && (jumpTimeCounter == jumpTime)) {// faire sauter Ryu
             rbRyu.velocity = new Vector2(rbRyu.velocity.x, jumpForce);
             isJumping = true;
             ryuAnimator.SetBool("isJumping", true);
+            grounded = false;
         } else if (Input.GetKey("z") && isJumping) { // augmente le jump quand rester appuyer
             if (jumpTimeCounter > 0) {
                 rbRyu.velocity = new Vector2(rbRyu.velocity.x, jumpForce);
@@ -56,6 +90,53 @@ public class BasicMoveP1 : MonoBehaviour {
             }
         } else {
             ryuAnimator.SetBool("isJumping", false);
+        }
+
+        if (startDelay) {
+            timePassed += Time.deltaTime;
+            if (timePassed <= delay) {
+                if (rightPress >= 2 && leftPress == 0 && grounded) {
+                    isDashingRight = true;
+                    rightPress = 0;
+                } else if (leftPress >= 2 && rightPress == 0 && grounded) {
+                    isDashingLeft = true;
+                    leftPress = 0;
+                }
+            } else {
+                timePassed = 0;
+                startDelay = false;
+                rightPress = 0;
+                leftPress = 0;
+            }
+        }
+
+        if (isDashingRight) {
+            if (dashTimeCounter <= dashTime) {
+                transform.Translate(Vector2.right * (speed * 3) * Time.deltaTime);
+                dashTimeCounter += Time.deltaTime;
+            } else {
+                dashTimeCounter = 0;
+                isDashingRight = false;
+            }
+        } else if (isDashingLeft) {
+            if (dashTimeCounter <= dashTime) {
+                transform.Translate(-Vector2.left * (speed * 3) * Time.deltaTime);
+                dashTimeCounter += Time.deltaTime;
+            } else {
+                dashTimeCounter = 0;
+                isDashingLeft = false;
+            }
+        }
+
+        if (isRight && !grounded) {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        } else if (isLeft && !grounded) {
+            transform.Translate(-Vector2.left * speed * Time.deltaTime);
+        }
+
+        if ((!Input.GetKey("q") || !Input.GetKey("d")) && grounded) {
+            isLeft = false;
+            isRight = false;
         }
 
         if (Input.GetKeyUp("z")) { // empeche de reaugmenter le jump après stop jump
@@ -80,10 +161,33 @@ public class BasicMoveP1 : MonoBehaviour {
         }
     }
 
+    /*private void FixedUpdate() {
+        if (startDelay) {
+            timePassed += Time.deltaTime;
+            if (timePassed <= delay) {
+                if (rightPress >= 2) {
+                    rbRyu.velocity = new Vector2(speed * 2, rbRyu.velocity.y);
+                    rightPress = 0;
+                }
+                else if (leftPress >= 2) {
+                    rbRyu.velocity = new Vector2(-(speed * 2), rbRyu.velocity.y);
+                    leftPress = 0;
+                }
+            }
+            else {
+                timePassed = 0;
+                startDelay = false;
+                rightPress = 0;
+                leftPress = 0;
+            }
+        }
+    }*/
+
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "ground") {
             isJumping = false;
             jumpTimeCounter = jumpTime;
+            grounded = true;
         }
     }
 }
--- CrippleFight/Assets/Scripts/BasicMoveP2.cs
+++ CrippleFight/Assets/Scripts/BasicMoveP2.cs
@@ -6,10 +6,12 @@ public class BasicMoveP2 : MonoBehaviour {
 
     public int speed; // speed de déplacement de Ryu
     public int jumpForce; // speed de jump
+    private int rightPress, leftPress; // nb de fois qu'on a appué sur left ou right
     public float jumpTime; //nb de sec max du jump
     public float jumpTimeCounter; //compteur du temps de jump
+    private float delay, delayPress, timePassed, timePassedPress, dashTime, dashTimeCounter;
     public Rigidbody2D rbRyu; // pour sauter
-    private bool isJumping = false;
+    private bool isJumping = false, startTimer, startDelay, grounded, isRight, isLeft, isDashingRight, isDashingLeft;
     public Animator ryuAnimator;
     Vector2 Pos, Max;
 
@@ -17,27 +19,59 @@ public class BasicMoveP2 : MonoBehaviour {
     void Start() {
         jumpTimeCounter = jumpTime;
         ryuAnimator = GetComponent<Animator>();
+        rightPress = leftPress = 0;
+        timePassed = timePassedPress = dashTimeCounter = 0;
+        delayPress = 0.25f;
+        delay = 0.25f;
+        dashTime = 0.1f;
+        isDashingLeft = isDashingRight = isRight = isLeft = false;
         Vector3 Cam = Camera.main.transform.position;
         Max = Camera.main.ScreenToWorldPoint(Cam);
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKey("right") && transform.position.x <= 10.45f && ryuAnimator.GetBool("isCrouching") == false) {
+        if (Input.GetKeyDown("right")) {
+            rightPress++;
+            startTimer = true;
+        } else if (Input.GetKeyDown("left")) {
+            leftPress++;
+            startTimer = true;
+        }
+
+        if (startTimer) {
+            timePassedPress += Time.deltaTime;
+            if (timePassedPress >= delayPress) {
+                startTimer = false;
+                rightPress = 0;
+                leftPress = 0;
+                timePassedPress = 0;
+            }
+        }
+
+        if (leftPress >= 2 || rightPress >= 2) {
+            startDelay = true;
+        }
+
+        if (Input.GetKey("right") && transform.position.x <= 10.45f && ryuAnimator.GetBool("isCrouching") == false && grounded) {
             rbRyu.transform.rotation = new Quaternion(0, 180f, 0f, 0f);
             transform.Translate(-Vector2.right * speed * Time.deltaTime); // faire avancer Ryu à droite
             Pos = transform.position;
             Pos.x = Mathf.Clamp(Pos.x, Max.x - Max.x / 10, -Max.x + Max.x / 10);
             transform.position = Pos;
             ryuAnimator.SetBool("isWalking", true);
+            isRight = true;
+            isLeft = false;
 
-        } else if (Input.GetKey("left") && transform.position.x >= -10.40f && ryuAnimator.GetBool("isCrouching") == false) {
+        } else if (Input.GetKey("left") && transform.position.x >= -10.40f && ryuAnimator.GetBool("isCrouching") == false && grounded) {
             rbRyu.transform.rotation = new Quaternion(0, 0f, 0f, 0f);
             transform.Translate(Vector2.left * speed * Time.deltaTime); // faire avancer Ryu à gauche
             Pos = transform.position;
             Pos.x = Mathf.Clamp(Pos.x, Max.x - Max.x / 10, -Max.x + Max.x / 10);
             transform.position = Pos;
             ryuAnimator.SetBool("isWalking", true);
+            isLeft = true;
+            isRight = false;
         } else {
             ryuAnimator.SetBool("isWalking", false);
         }
@@ -46,6 +80,7 @@ public class BasicMoveP2 : MonoBehaviour {
             rbRyu.velocity = new Vector2(rbRyu.velocity.x, jumpForce);
             isJumping = true;
             ryuAnimator.SetBool("isJumping", true);
+            grounded = false;
         } else if (Input.GetKey("up") && isJumping) { // augmente le jump quand rester appuyer
             if (jumpTimeCounter > 0) {
                 rbRyu.velocity = new Vector2(rbRyu.velocity.x, jumpForce);
@@ -54,6 +89,53 @@ public class BasicMoveP2 : MonoBehaviour {
             }
         } else {
             ryuAnimator.SetBool("isJumping", false);
+        }
+
+        if (startDelay) {
+            timePassed += Time.deltaTime;
+            if (timePassed <= delay) {
+                if (rightPress >= 2 && leftPress == 0 && grounded) {
+                    isDashingRight = true;
+                    rightPress = 0;
+                } else if (leftPress >= 2 && rightPress == 0 && grounded) {
+                    isDashingLeft = true;
+                    leftPress = 0;
+                }
+            } else {
+                timePassed = 0;
+                startDelay = false;
+                rightPress = 0;
+                leftPress = 0;
+            }
+        }
+
+        if (isDashingRight) {
+            if (dashTimeCounter <= dashTime) {
+                transform.Translate(-Vector2.right * (speed * 3) * Time.deltaTime);
+                dashTimeCounter += Time.deltaTime;
+            } else {
+                dashTimeCounter = 0;
+                isDashingRight = false;
+            }
+        } else if (isDashingLeft) {
+            if (dashTimeCounter <= dashTime) {
+                transform.Translate(Vector2.left * (speed * 3) * Time.deltaTime);
+                dashTimeCounter += Time.deltaTime;
+            } else {
+                dashTimeCounter = 0;
+                isDashingLeft = false;
+            }
+        }
+
+        if (isRight && !grounded) {
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);
+        } else if (isLeft && !grounded) {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+
+        if ((!Input.GetKey("right") || !Input.GetKey("left")) && grounded) {
+            isLeft = false;
+            isRight = false;
         }
 
         if (Input.GetKeyUp("up")) { // empeche de reaugmenter le jump après stop jump
@@ -82,6 +164,7 @@ public class BasicMoveP2 : MonoBehaviour {
         if (collision.gameObject.tag == "ground") {
             isJumping = false;
             jumpTimeCounter = jumpTime;
+            grounded = true;
         }
     }
 }
