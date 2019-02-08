bc91795c3794e37a982ca0f789df529dfb71e7e5
Byzness <guillaume.briant@free.fr>
2019-02-08 22:18:59 +0100

    Musiques menu et combat

---
+++ CrippleFight/Assets/Scripts/AudioManagerFight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManagerFight : MonoBehaviour {
+
+    public AudioClip themeLoop;
+    public AudioSource me;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!me.isPlaying) {
+            me.PlayOneShot(themeLoop);
+        }
+	}
+}
