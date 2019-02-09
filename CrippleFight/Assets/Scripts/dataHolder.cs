4820af478d52d36e9b8fc66c051874396bba7bfe
Byzness <guillaume.briant@free.fr>
2019-02-09 16:45:33 +0100

    oubli prefabs + scripts dernier push

---
+++ CrippleFight/Assets/Scripts/dataHolder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dataHolder : MonoBehaviour {
+
+    private static bool fromMenuButton;
+
+    public static bool FromMenuButton {
+        get {
+            return fromMenuButton;
+        }
+        set {
+            fromMenuButton = value;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
